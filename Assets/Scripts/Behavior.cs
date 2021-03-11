1dd6a256aec71f7cced79b867d2656ad2a0d84d8
Nikita Alexandrov <nikitosiks338@gmail.com>
2021-03-11 14:24:10 +0300

    some ideas

---
+++ Assets/Scripts/Behavior.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Behavior
+{
+    public string Tag;
+
+    private Dictionary<string, Dictionary<string, Action>> reactionTable;
+
+    public void ReactTo(string otherTag, string otherMessage, Sim other)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/Sim.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class Sim : MonoBehaviour
+{
+    public Behavior Behavior;
+
+    public void CalledBy(Sim other, string message)
+    {
+        throw new NotImplementedException();
+    }
+}
