b1ef7b7e7d20dded67b3a7531b938b255f763200
Nikita Alexandrov <nikitosiks338@gmail.com>
2021-03-12 09:23:53 +0300

    Property declaration

--- Assets/Scripts/LifeProperties.cs
+++ Assets/Scripts/LifeProperties.cs
@@ -3,22 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Simulation
 {
+    using Property = System.String;
     public class LifeProperties
     {
-        public Dictionary<string, int> Props;
-        public void AddProperty(string name, int value_by_default)
+        public Dictionary<Property, int> Props;
+        public void AddProperty(Property name, int value_by_default)
         {
             throw new NotImplementedException();
         }
 
-        public int GetPropertyValue(string name)
+        public void ChangeValue(PropertyAttribute name, int value)
         {
             throw new NotImplementedException();
         }
-        
-        
+
+        public int GetPropertyValue(Property name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsPropertyIn(Property name)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
